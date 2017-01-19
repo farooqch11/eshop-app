401cbf4ec010abbad481e67f1b3d627e9ac034fe
Farooq <farooqch11@gmail.com>
2017-01-20 03:19:42 +0500

    custom admin pannel

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation,:role
 
   index do
     selectable_column
@@ -11,18 +11,5 @@ ActiveAdmin.register User do
     actions
   end
 
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
-
-  form do |f|
-    f.inputs "Admin Details" do
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-    end
-    f.actions
-  end
 
 end
